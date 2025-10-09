bce2dbf7707a4ba1979fb0d6a4ee835a19e9ae23
Joanjo17 <joanlica04@hotmail.com>
2025-10-09 06:00:24 +0200

    bootstrap del proyecto(Estructura + dependencias + readme)

---
+++ src/test/java/com/fitnessapp/fitapp_api/FitappApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitnessapp.fitapp_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitappApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

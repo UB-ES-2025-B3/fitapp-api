6d7e34687d943727b9a83e937e7533b3d3039c70
Joanjo17 <joanlica04@hotmail.com>
2025-10-13 14:19:31 +0200

    feat(security,core):Cambio en las URLs de autenticación permitidas, cambios de "/api/auth/*" a "/api/v1/auth/*".

--- src/main/java/com/fitnessapp/fitapp_api/core/security/SecurityConfig.java
+++ src/main/java/com/fitnessapp/fitapp_api/core/security/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider())
                 .authorizeHttpRequests(reg -> reg
-                        .requestMatchers(HttpMethod.POST, "/api/auth/register", "/api/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/auth/register", "/api/v1/auth/login").permitAll()
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyRequest().authenticated()       // o .permitAll() para no exigir auth a nivel URL

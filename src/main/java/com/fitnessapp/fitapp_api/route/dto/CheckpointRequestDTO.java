720b964eb12c0a8f6cdcf802779660cf2330983b
Adriidomiingo <adridomi04@gmail.com>
2025-11-20 21:37:40 +0100

    tests unitarios para checkpoints

--- src/main/java/com/fitnessapp/fitapp_api/route/dto/CheckpointRequestDTO.java
+++ src/main/java/com/fitnessapp/fitapp_api/route/dto/CheckpointRequestDTO.java
@@ -3,6 +3,8 @@ package com.fitnessapp.fitapp_api.route.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public record CheckpointRequestDTO(
     @Schema(
             description = "Nombre del checkpoint",
@@ -16,6 +18,7 @@ public record CheckpointRequestDTO(
             example = "41.00001,-8.50002"
     )
     @NotBlank(message = "Checkpoint point is required")
+    @Pattern(regexp = "^-?\\d{1,2}\\.\\d+,-?\\d{1,3}\\.\\d+$", message = "Point must be in 'lat,lon' format")
     String point
 ){
 }

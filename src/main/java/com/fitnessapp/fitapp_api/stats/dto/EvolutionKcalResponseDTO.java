bc27ef85cf0d69bd6d302d6c2808699c0b800093
Adriidomiingo <adridomi04@gmail.com>
2025-11-22 19:58:01 +0100

    unit tests para evolution dailyKcal

--- src/main/java/com/fitnessapp/fitapp_api/stats/dto/EvolutionKcalResponseDTO.java
+++ src/main/java/com/fitnessapp/fitapp_api/stats/dto/EvolutionKcalResponseDTO.java
@@ -3,6 +3,6 @@ package com.fitnessapp.fitapp_api.stats.dto;
 import java.util.List;
 
 public record EvolutionKcalResponseDTO(
-        List<DailyKcalResponseDTO> checkpoints
+        List<DailyKcalResponseDTO> points
 ) {
 }

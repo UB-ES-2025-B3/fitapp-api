22e6fd707e7171f15bf5a55ea046a013d089788f
Joanjo17 <joanlica04@hotmail.com>
2025-12-06 21:34:15 +0100

    Error de sintaxis. faltaba un '}' en CalorieCalculationService.

--- src/main/java/com/fitnessapp/fitapp_api/calories/service/CalorieCalculationService.java
+++ src/main/java/com/fitnessapp/fitapp_api/calories/service/CalorieCalculationService.java
@@ -6,6 +6,7 @@ import com.fitnessapp.fitapp_api.profile.model.UserProfile;
 
 public interface CalorieCalculationService {
     double calculateCalories(UserProfile profile, CCActivityRequest activity);
+
     /**
      * Checks if the user has reached their daily calorie goal.
      * Calculates the calories burned for the current day according to the user's profile timezone.
@@ -14,3 +15,4 @@ public interface CalorieCalculationService {
      * @return true if the calories burned today are greater than or equal to the daily goal, false otherwise
      */
     boolean hasReachedDailyGoal(UserProfile profile);
+}
